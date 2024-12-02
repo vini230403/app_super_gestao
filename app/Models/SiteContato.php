8ddc391d2f43eea4d4211d4c121d12b277c52367
vini230403 <vini2003.melo@gmail.com>
2024-12-02 13:54:34 -0300

    Customizando as mensagens de feedback de validação

--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
-    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contatos_id', 'mensagem'];
     use HasFactory;
 }
